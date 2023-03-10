b5c928a7559250e5e83532c3774c0abb8f730a9a
Christopher Wech <christopherwech@gmail.com>
2023-03-10 14:47:35 -0600

    created content for Header.js

---
+++ client/src/components/Header.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { AppBar, Tab, Tabs, Toolbar, Typography } from "@mui/material";
+import LibraryBooksOutlinedIcon from "@mui/icons-material/LibraryBooksOutlined";
+import { NavLink } from "react-router-dom";
+const Header = () => {
+  const [value, setValue] = useState();
+  return (
+    <div>
+      <AppBar sx={{ backgroundColor: "blue" }} position="sticky">
+        <Toolbar>
+          <NavLink to="/" style={{ color: "white" }}>
+            <Typography>
+              <LibraryBooksOutlinedIcon />
+            </Typography>
+          </NavLink>
+          <Tabs 
+            sx={{ ml: "auto" }}
+            textColor="inherit"
+            indicatorColor="primary"
+            value={value}
+            onChange={(e, val) => setValue(val)}
+          >
+            <Tab LinkComponent={NavLink} to="/add" label="Add Patient" />
+            <Tab LinkComponent={NavLink} to="/patient" label="Patient Information" />
+            <Tab LinkComponent={NavLink} to="/about" label="About MERN Health" />
+          </Tabs>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+export default Header;
