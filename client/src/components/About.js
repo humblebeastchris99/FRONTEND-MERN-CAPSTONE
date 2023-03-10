b824c6e03aab3aaa6349955c439f86a828f494ac
Christopher Wech <christopherwech@gmail.com>
2023-03-10 14:51:58 -0600

    created content for About.js

---
+++ client/src/components/About.js
@@ -0,0 +1,82 @@
+import hospital from "../images/hospital_staff.jpg"
+  
+import React from "react";
+const About = () => {
+  return (
+    <div>
+    
+        <div>
+            <h1>About Cape Health Medical Staff</h1>
+            <img src={hospital} alt= "hospital staff"></img>
+        </div>
+
+      <p>Cape Health Hospital’s staff of more than 1,900 caring and compassionate individuals focus on the hospital’s mission to 
+        deliver compassionate clinical care, conduct innovative scientific discovery, provide comprehensive professional training,
+         and offer robust public education.Our board of directors is made up of community leaders who are experts in health care and business. They provide valuable guidance that shapes the future of Emory Healthcare,
+        ensuring outstanding care now and for many years to come.Call Us
+        
+        
+        Contact Cape Health Children's Hospital
+        capehealthpediatrics@aol.com
+        954-265-5322</p>
+
+        Cape Health — one of the region's leading pediatric hospitals — is trusted by South Florida families in the moments that count. 
+        We offer a comprehensive scope of healthcare services and programs in a child-friendly atmosphere.
+         As a full-service hospital, we treat minor illnesses, trauma-related accidents and some of the most complex medical conditions.
+
+
+Established in 2022, Cape Health Hospital combines advanced technology and the expertise of the largest, most diverse group of board-certified pediatric specialists in the region. To meet the growing needs of the community,
+ the hospital expanded in 2011 and again in 2022.Cape Health Hospital now has 298 beds.
+
+In its first year of operation, Cape Health medical staff included 90 physicians and patient admissions numbered 2,000. Through the years, the hospital has enhanced and expanded its services and programs to help meet the growing and diverse needs of the community.
+
+The hospital is staffed 24 hours a day by world-class pediatricians, pediatric specialists, specialty-trained nurses and ancillary support staff. Today, more than 650 physicians are on the medical staff.
+
+Number of Visits
+70,642
+Admissions
+Jul 2021 - Jun 2022The total number of pediatric patients who are seeking medical services at the facility during the time indicated. Includes patients categorized as "inpatient" meaning that the patient remains in the hospital for at least one night, as well as patients on "observation status" meaning that the patient is being monitored on an ongoing basis in order to assess the patient's medical condition.
+8,201
+Emergency Department Visits
+Jul 2021 - Jun 2022The total number of pediatric patients who entered the Emergency Department to seek treatment during the time period indicated.
+8,201
+Total Surgeries
+Jul 2021 - Jun 2022The total number of surgical procedures received by patients during the time indicated. Includes both inpatient and outpatient surgical procedures.
+
+About Our Location
+
+Take a tour of Cape Health Hospital in Hollywood, Florida
+Uniquely inspired by and designed for kids and families, the freestanding building offers many amenities. The vibrant colors, whimsical décor and larger-than-life murals welcome children to an upbeat environment where the "Power of Play" is a healing force. Each floor reflects an individual theme of sports, arts, games or dreams. All patient rooms are private and are wired for movies, video games, the Internet and educational programs, and room-service meals are available any time of the day.
+
+The eight-story, nearly 400,000-square-foot, environmentally friendly facility features:
+
+State-of-the-art patient rooms
+Cardiac hybrid operating rooms
+Surgical and interventional radiology suites
+Intensive care and cardiac intensive care units
+Intraoperative MRI
+Family resource and respite spaces
+Ambulatory services
+Imaging services that feature a CT scanner disguised as a pirate ship
+Inpatient and outpatient pediatric oncology treatment 
+A glass pedestrian skywalk connects to the Wasie Neonatal Intensive Care Unit, Pediatric Intensive/Cardiac Care Unit, and Level 1 Trauma Center and Children's Emergency Department.
+
+Conveniently located on the campus, Conine Clubhouse provides a special home away from home for families of hospitalized children. Accommodations are based on availability.
+
+The team at Joe DiMaggio Children's Hospital embraces a patient- and family-centered approach to care, working with families in the effort to best meet the needs of the child.
+
+Call Us
+Contact Cape Health Hospital.
+
+954-265-5322
+IN THIS SECTION
+Our HistoryWhy Choose UsOur ApproachAccessibility StatementIn the CommunityOur FoundationGet InvolvedFind a PhysicianView Healthcare Prices
+News and Patient Stories
+cath the love classic golf 2023
+"Catch the Love Classic" Raises $420,000 for Cape Health Children's Hospital Foundation
+
+     </div>
+    
+  );
+};
+export default About;
